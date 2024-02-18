95e6e61ec404ca25136ebde705953f65a6c80dbe
Felix-Kariuki <felixmurimi280@gmail.com>
2024-02-18 16:33:31 +0300

    Initial Commit

---
+++ Tasky/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tasky
+//
+//  Created by Felix kariuki on 18/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tasky/TaskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskyApp.swift
+//  Tasky
+//
+//  Created by Felix kariuki on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
